3c4977dce354eb5543033d16602befdf1071c67e
Mary <68235230+maryjng@users.noreply.github.com>
2022-09-27 03:33:52 -0400

    Delete src directory

--- src/AppointmentPage.js
+++
@@ -1,1 +0,0 @@
-// component for an appointment showing weather forecast for the day and options like changing details and inviting others
--- src/Calendar.js
+++
@@ -1,3 +0,0 @@
-//Displays a calendar view, starting with current month. Days with appointments will include another component with appt summary(name) and location. 
-//The appointment's zipcode will be used to request the weather for that day. The weather icon from the same request will be used on this calendar view. 
-//There will be a link to a AppointmentPage component
