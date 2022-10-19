5e943497b48dcd77f7438e9edfae7c781fa6546f
Mary <68235230+maryjng@users.noreply.github.com>
2022-10-19 11:00:38 -0400

    Add files via upload

--- src/HomeCalendar.js
+++ src/HomeCalendar.js
@@ -7,13 +7,14 @@ import PlannerApi from "./api";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import moment from 'moment';
 
 function HomeCalendar({ allEvents, handleEditEvent, handleDeleteEvent }) {
     const navigate = useNavigate()
     const [apptForecast, setApptForecast] = useState([])
     const [apptDetails, setApptDetails] = useState("")
-    
-  // upon click of event, fetch stored forecast and display it through apptForecast component. Takes the appt info and sets the state for apptDetails
+
+  // upon click of appt on calendar, fetch stored forecast and display it through apptForecast component. Takes the appt info and sets the state for apptDetails
     async function handleSelected(event) {
         let appt = await PlannerApi.getAppt(event.id)
                 
@@ -22,9 +23,7 @@ function HomeCalendar({ allEvents, handleEditEvent, handleDeleteEvent }) {
             appt.zipcode = ("0" * (5 - appt.zipcode.length)) + appt.zipcode
         } 
         setApptDetails(appt)
-
         let forecast = await PlannerApi.getApptForecast(event.id)
-        console.log(forecast)
         setApptForecast(forecast)
     }
 
@@ -35,19 +34,46 @@ function HomeCalendar({ allEvents, handleEditEvent, handleDeleteEvent }) {
         return result;
     }
 
-    function showEditForm() {
-        return (
-            <EditApptForm handleEditEvent={handleEditEvent} appt_id={apptDetails.id} />
-        )
+    async function updateForecast() {
+        let today = new Date()
+
+        //make today the startDate if appt start date is before today (clears up the forecast and db and doesn't matter anyway)
+        let startDate = new Date(apptDetails.startdate.slice(0, 10))
+        if (startDate < today) {
+            startDate = today
+        }
+
+        //clear all the old forecasts first
+        PlannerApi.deleteAllForecasts(apptDetails.id)
+
+        const res = await PlannerApi.getForecast({ 
+            "zipcode": apptDetails.zipcode, 
+            "tempUnit": "fahrenheit", 
+            "startDate": startDate, 
+            "endDate": apptDetails.enddate
+        })
+
+        //save each day's forecast to db.
+        //Checking if max_temp is included is a temp fix because for some reason the first day does not get any forecast results from the third party weather api
+        for (const key in res) {
+            if (res[key].max_temp) {
+            await PlannerApi.addForecast(apptDetails.id, res[key])
+            }
+        }
+
+        //refresh the displayed forecast state
+        const forecast = await PlannerApi.getApptForecast(apptDetails.id)
+        setApptForecast(forecast)
     }
 
-    //make date and time look nice for display
+    //add back offset to db-stored UTC datetime
     function convToDateAndTime(t) {
-        let date = t.slice(0, 10)
-        let hour = (t.slice(11, 13) >= 12) ? (t.slice(11, 13) - 12) : t.slice(11, 13)
-        let minute = t.slice(14, 16)
-        let ampm = hour > 0 ? "PM" : "AM"
-        return date + " " + hour + ":" + minute +  " " + ampm
+        let d = new Date()
+        const o = d.getTimezoneOffset()
+
+        t = moment(t)
+        t.subtract(o, 'm')
+        return t.toString()
     }
 
     return(
@@ -56,15 +82,12 @@ function HomeCalendar({ allEvents, handleEditEvent, handleDeleteEvent }) {
 
             <Container>
                 <Row>
-                    <Col sm={7}>
-                        <div style={{textAlign:"center"}}>
-                            <h3>Appointment Details</h3>
-                            <p>Click on an appointment on the calendar above to view its details below. You will also be able to edit the details on the right.</p>
-                            <h5><i>Saved Forecast</i></h5>
-                        </div>
-                    
+                    <Col sm={7} style={{padding: "0px"}}>
+                        <h3>View Appointment Details and Saved Forecasts</h3>
+                        <p><i>Saved Forecasts</i></p>
+                        <button onClick={updateForecast}>Update Forecast</button>
                         <ApptDetail apptForecast={apptForecast} />
-                        <div>
+                         <div>
                             <div>Title: {apptDetails.title}</div>
                             <div>Start: {apptDetails.startdate ? convToDateAndTime(apptDetails.startdate) : ""}</div>
                             <div>End: {apptDetails.enddate ? convToDateAndTime(apptDetails.enddate) : ""}</div>
@@ -74,12 +97,11 @@ function HomeCalendar({ allEvents, handleEditEvent, handleDeleteEvent }) {
                         </div>
 
                         <div style={{alignContent: "right"}}>
-                            {/* <button onClick={showEditForm}>Edit Appointment</button>  */}
-                            <button onClick={handleDelete}>Delete Appointment</button>
+                            <button onClick={handleDelete} style={{marginTop: "10px"}}>Delete Appointment</button>
                         </div>
                     </Col>
                     <Col sm={5}>
-                        {apptDetails !== "" ? showEditForm() : ''}
+                        <EditApptForm handleEditEvent={handleEditEvent} setApptDetails={setApptDetails} appt_id={apptDetails.id} />
                     </Col>
                 </Row>
             </Container>
